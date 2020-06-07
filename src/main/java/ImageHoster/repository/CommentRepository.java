b810b68c477a7e8e2bc6137d7d4a4f45f345062c
Tayyab Khan <tkhan@atlassian.com>
2020-06-07 14:00:38 +0530

    Added Working comment repository

---
+++ src/main/java/ImageHoster/repository/CommentRepository.java
@@ -0,0 +1,28 @@
+package ImageHoster.repository;
+
+import ImageHoster.model.Comment;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CommentRepository {
+
+  @PersistenceUnit(unitName = "imageHoster")
+  private EntityManagerFactory emf;
+
+  public void updateImageComment(Comment comment) {
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction transaction = em.getTransaction();
+
+    try {
+      transaction.begin();
+      em.persist(comment);
+      transaction.commit();
+    } catch (Exception e) {
+      transaction.rollback();
+    }
+  }
+}
